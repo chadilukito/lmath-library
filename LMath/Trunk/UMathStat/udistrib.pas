4b8997ae20b6416dd43e7b6b57634c179fcb7c8b
glorfin <glorfin@83f5119b-32fd-4276-87c0-772de1ac6cd8>
2020-06-13 00:34:19 +0000

    distExtract* procedures added

--- LMath/Trunk/UMathStat/udistrib.pas
+++ LMath/Trunk/UMathStat/udistrib.pas
@@ -11,25 +11,38 @@ interface
 uses
   utypes;
 
-{ Allocates an array of statistical classes (histogram bins).
-A is lower border of histogram; B is upper border; H is bin width.
-Function calculates number of bins and allocates them. Number of bins is returned.
-If allocation is impossible, nil is returned.}
+//Allocates an array of statistical classes (histogram bins).
+//A is lower border of histogram; B is upper border; H is bin width.
+//Function calculates number of bins and allocates them. Number of bins is returned.
+//If allocation is impossible, nil is returned.}
 function DimStatClassVector(out C : TStatClassVector; A, B, H : float):integer;
 
-{ Distributes the values of array X[Lb..Ub] into M classes with
-  equal width H, according to the following scheme:
-
-              C[1]    C[2]                    C[M]
-           ]-------]-------].......]-------]-------]
-           A      A+H     A+2H                     B
-
-  such that B = A + M * H }
+ //Distributes the values of array X[Lb..Ub] into M classes with
+ // equal width H, according to the following scheme:
+ //
+ //             C[1]    C[2]                    C[M]
+ //          ]-------]-------].......]-------]-------]
+ //          A      A+H     A+2H                     B
+ //
+ // such that B = A + M * H
 procedure Distrib(X       : TVector;
                   Lb, Ub  : Integer;
                   A       : Float; // A:lower border of histogram. Upper one is found from length of C.
                   H       : Float; // H is bin width. Number of bins is calculated
                   C       : TStatClassVector);
+
+// Extacts from StatClassVector a vector of X-values for a histogram (middles of each bin)
+procedure distExtractX(C : TStatClassVector; out Xv : TVector);
+
+// Extacts from StatClassVector a vector of N (counts) for each bin
+procedure distExtractN(C : TStatClassVector; out Nv : TIntVector);
+
+// Extacts from StatClassVector a vector of Frequency
+procedure distExtractFreq(C : TStatClassVector; out Fv : TVector);
+
+// Extacts from StatClassVector a vector of probability density
+procedure distExtractDensity(C : TStatClassVector; out Dv : TVector);
+
 
 implementation
 
@@ -84,6 +97,42 @@ begin
         F := N / Nt;
         D := F / H;
       end;
+end;
+
+procedure distExtractX(C: TStatClassVector; out Xv: TVector);
+var
+  I:Integer;
+begin
+  DimVector(Xv,high(C));
+  for I := 1 to high(C) do
+    Xv[I] := (C[I].Sup - C[I].Inf)/2;
+end;
+
+procedure distExtractN(C: TStatClassVector; out Nv: TIntVector);
+var
+  I:Integer;
+begin
+  DimVector(Nv,high(C));
+  for I := 1 to high(C) do
+    Nv[I] := C[I].N;
+end;
+
+procedure distExtractFreq(C: TStatClassVector; out Fv: TVector);
+var
+  I:Integer;
+begin
+  DimVector(Fv,high(C));
+  for I := 1 to high(C) do
+    Fv[I] := C[I].F;
+end;
+
+procedure distExtractDensity(C: TStatClassVector; out Dv: TVector);
+var
+  I:Integer;
+begin
+  DimVector(Dv,high(C));
+  for I := 1 to high(C) do
+    Dv[I] := C[I].D;
 end;
 
 end.
